e4e7aa8b59a9c76f19ac9919a44c8febac266083
Alejandro Cordero <acordero@piconsulting.com.ar>
2026-09-21 22:34:46 -0300

    Fix unused registration import

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { FormEvent, useState } from 'react';
-import { registerUser, saveSession } from '@/lib/app-state';
+import { saveSession } from '@/lib/app-state';
 
 export default function RegisterPage() {
   const router = useRouter();
